bdbcb91a24f94d87a7aa0a25a1470fb9eb06f8d5
monika2409 <42198703+monika2409@users.noreply.github.com>
2018-08-14 19:47:06 +0530

    Add files via upload

---
+++ Project/src/ExcelDataConfig.java
@@ -0,0 +1,69 @@
+package Aprimo;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelDataConfig 
+{
+
+	XSSFWorkbook wb;
+	XSSFSheet sheet1;
+	
+	public ExcelDataConfig(String excelPath)
+	{
+
+		try 
+		{
+			File src = new File(excelPath);
+			FileInputStream fis = new FileInputStream(src);
+			wb = new XSSFWorkbook(fis);
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public String getData(int sheetNumber, int row,int column)
+	{
+	  sheet1 = wb.getSheetAt(sheetNumber);
+	  DataFormatter formatter = new DataFormatter();
+	  String data = formatter.formatCellValue(sheet1.getRow(row).getCell(column));
+	 // String data = sheet1.getRow(row).getCell(column).getStringCellValue();
+	  
+	  return data;
+	}
+	
+	/*public double getDoubleData(int sheetNumber, int row,int column)
+	{
+	  sheet1 = wb.getSheetAt(sheetNumber);
+	  double data = sheet1.getRow(row).getCell(column).getNumericCellValue();
+	  
+	  return data;
+	}*/
+	  
+	/*public int getData1(int sheetNumber, int row,int column)
+	  {
+	  sheet1 = wb.getSheetAt(sheetNumber);
+	  int data1 = (int)sheet1.getRow(row).getCell(column).getNumericCellValue();
+	  return data1;
+	  //System.out.println(data1);
+	  }*/
+	  
+	public int getRowCount(int sheetIndex) 
+	{
+		int row =  wb.getSheetAt(sheetIndex).getLastRowNum();
+		row=row + 1;
+		return row;
+	}
+	  
+	/* public int getColumnCount(int sheetIndex) {
+	int column = wb.getSheetAt(sheetIndex).getColumnWidth(column);
+	return column;
+	 
+	}*/
+}
---
+++ Project/src/ReadfromExcel.java
@@ -0,0 +1,68 @@
+package Aprimo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+//import Aprimo.ExcelDataConfig;
+
+public class ReadfromExcel 
+{
+	WebDriver driver;
+	
+	@Test(dataProvider = "testdata")
+	public void setup(String Firstname,String Lastname, String Email, String phone) throws InterruptedException 
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\monika\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		driver=new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.get("https://staging.aprimodm.com/Login2.aspx?ReturnUrl=/");
+		
+		//String a = driver.findElement(By.className("fancybox-iframe")).getAttribute("name");
+		//System.out.println(a);
+		
+		driver.findElement(By.id("lnksignon")).click();
+		driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[class='fancybox-iframe']")));
+		driver.findElement(By.id("txtUserFName")).sendKeys(Firstname);
+		driver.findElement(By.id("txtUserLName")).sendKeys(Lastname);
+		driver.findElement(By.id("txtUserEmail")).sendKeys(Email);
+		driver.findElement(By.id("txtUserPhoneNo")).sendKeys(phone);
+		
+		//driver.findElement(By.id("txtUserPhoneNo")).sendKeys(String.valueOf(phone));
+		//driver.findElement(By.cssSelector("input[name='txtUserPhoneNo']")).sendKeys(Integer.toString(phone));
+		driver.findElement(By.id("rbtnNewPartner")).isSelected();
+		
+		Thread.sleep(5000);
+	}
+	
+	@DataProvider(name="testdata")
+	public Object[][] passData()
+	{
+		ExcelDataConfig Config = new ExcelDataConfig("C:\\Users\\monika\\Desktop\\readexcel.xlsx");
+		
+		int rows = Config.getRowCount(0);
+		
+		Object[][] data = new Object[rows][4];
+		
+		for(int i = 1; i < rows; i++) 
+		{
+			data[i][0]= Config.getData(0, i, 0);
+			data[i][1]= Config.getData(0, i, 1);
+			data[i][2]= Config.getData(0, i, 2);
+			data[i][3]= Config.getData(0, i, 3);
+		}
+	
+		return data;
+	}
+	
+	@AfterMethod
+	public void Quit() 
+	{
+		driver.close();
+	}
+
+}
